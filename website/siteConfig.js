fa0d080643537aae59228f44ef73770d77bc3139
stahldom <jeff@helpfulcomm.com>
2020-01-23 13:49:26 +0100

    removed examples docs and built base structure for documentation

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,9 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'index', label: 'Docs'},
+    {doc: 'recipes/recipes', label: 'Recipes'},
+    {doc: 'tutorials/tutorials', label: 'Tutorials'}
   ],
 
   // If you have users set above, you add it here:
