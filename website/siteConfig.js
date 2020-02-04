a99ddeb7048bd30e0bea18147760236aa2628e87
stahldom <jeff@helpfulcomm.com>
2020-02-04 11:23:17 +0100

    added cname and updated to dev.datahighway.com

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Data Highway Developer Hub', // Title for your website.
   tagline: 'Complete develop documentation for the Data Highway DAO.',
-  url: 'https://DataHighway-com.github.io', // Your website URL
+  url: 'https://dev.datahighway.com', // Your website URL
   baseUrl: '/documentation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -32,6 +32,7 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'documentation',
   organizationName: 'DataHighway-com',
+  cname: 'dev.datahighway.com',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
