fedcafb845e89e43dc9e79e06e044705b985dc1d
Christian Gröschel <christian.groeschel@gmail.com>
2021-07-09 14:49:24 +0200

    disable agiola

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -47,11 +47,13 @@ const docusaurusConfig = {
     locales: ["en", "de", "zh-CN", "zh-TW"],
   },
   themeConfig: {
+    /**
+    * disabled until fixed
     algolia: {
       appId: process.env.ALGOLIA_APP_ID,
       apiKey: process.env.ALGOLIA_API_KEY,
       indexName: 'datahighway',
-    },
+    }, */
     liveCodeBlock: {
       /**
        * The position of the live playground, above or under the editor
